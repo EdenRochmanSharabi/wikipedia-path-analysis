f5b0a056cb2e41169d256d12ae109e060ecb0a26
Eden Rochman <eden@example.com>
2025-05-05 17:47:27 +0200

    Move all SQL and related files into misc_sql/ with subfolders for schema, admin, data, analysis, apex, and passwords. Clean up project root.

--- insert_sample_nodes.sql
+++
@@ -1,8 +0,0 @@
-alter session set container=XEPDB1;
-
-INSERT INTO system.wiki_path_nodes (node_id, path_id, step_number, article) VALUES (1, 1, 1, 'Philosophy');
-INSERT INTO system.wiki_path_nodes (node_id, path_id, step_number, article) VALUES (2, 1, 2, 'Reality');
-INSERT INTO system.wiki_path_nodes (node_id, path_id, step_number, article) VALUES (3, 1, 3, 'Science');
-INSERT INTO system.wiki_path_nodes (node_id, path_id, step_number, article) VALUES (4, 1, 4, 'Mathematics');
-
-COMMIT; 
